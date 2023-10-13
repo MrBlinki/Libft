2daf9ff13cd77caae3b261a8d6b270069e50c37f
Matthias Roth <maroth@student.42lausanne.ch>
2023-10-13 18:28:15 +0200

    ft_isdigit should be ok

--- tester.c
+++ tester.c
@@ -6,7 +6,7 @@
 /*   By: maroth <maroth@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 10:12:28 by maroth            #+#    #+#             */
-/*   Updated: 2023/10/13 11:41:56 by maroth           ###   ########.fr       */
+/*   Updated: 2023/10/13 18:27:16 by maroth           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,12 @@ int	main(void)
 	printf("ft_isalpha('Z'): 	%d\n", ft_isalpha('Z'));
 	printf("isalpha('A'): 		%d\n", isalpha('A'));
 	printf("ft_isalpha('A'): 	%d\n", ft_isalpha('A'));
+	printf("\n========== FT_ISDIGIT ==========\n");
+	printf("isdigit('0'):		%d\n", isdigit('0'));
+	printf("ft_isdigit('0'):	%d\n", ft_isdigit('0'));
+	printf("isdigit('a'):		%d\n", isdigit('a'));
+	printf("ft_isdigit('a'):	%d\n", ft_isdigit('a'));
+	printf("isdigit('9'):		%d\n", isdigit('9'));
+	printf("ft_isdigit('9'):	%d\n", ft_isdigit('9'));
 	return (0);
 }
