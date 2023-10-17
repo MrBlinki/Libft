2ceb30a003c48ac8d160a80f3e1bcdbb08ce91ff
Matthias Roth <maroth@student.42lausanne.ch>
2023-10-17 17:02:42 +0200

    Documented some functions

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,13 +6,13 @@
 /*   By: maroth <maroth@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 18:29:30 by maroth            #+#    #+#             */
-/*   Updated: 2023/10/15 16:03:32 by maroth           ###   ########.fr       */
+/*   Updated: 2023/10/17 16:58:54 by maroth           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// Returns non-zero if c is a letter or a digit, zero if not
 #include "libft.h"
 
+// Returns non-zero if c is a letter or a digit, zero if not
 int	ft_isalnum(int c)
 {
 	if (ft_isalpha(c) || ft_isdigit(c))
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: maroth <maroth@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 11:26:56 by maroth            #+#    #+#             */
-/*   Updated: 2023/10/13 14:13:20 by maroth           ###   ########.fr       */
+/*   Updated: 2023/10/17 17:01:26 by maroth           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <stdio.h>		// include for size_t type
 
 // Part1 - Libc functions
+
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
@@ -39,10 +40,12 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
 
 // Functions using malloc()
+
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
 
 // Part 2 - Additional functions
+
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
