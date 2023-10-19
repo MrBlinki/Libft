1d3f8749cad19ed34ed56cc8d64a615b4960dfd1
Matthias Roth <maroth@student.42lausanne.ch>
2023-10-19 13:20:07 +0200

    Changed ft_strdup so it nul-terminates the copy

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,22 +6,23 @@
 /*   By: maroth <maroth@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 14:33:19 by maroth            #+#    #+#             */
-/*   Updated: 2023/10/18 15:12:24 by maroth           ###   ########.fr       */
+/*   Updated: 2023/10/19 13:06:50 by maroth           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// Returns a pointer to copy of s1 in allocated memory
 char	*ft_strdup(const char *s1)
 {
 	char	*ptr;
 	size_t	len;
 
 	ptr = (void *)0;
-	len = ft_strlen(s1);
+	len = ft_strlen(s1) + 1;
 	ptr = ft_calloc(len, sizeof(char));
 	if (!ptr)
 		return ((void *)0);
-	ft_memmove(ptr, s1, len);
+	ft_strlcpy(ptr, s1, len);
 	return (ptr);
 }
