3de66da64add8bf247799356500083fb63c78701
Matthias Roth <maroth@student.42lausanne.ch>
2023-10-27 21:05:11 +0200

    ft_lstlast now handles null as list

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: maroth <maroth@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 20:25:35 by maroth            #+#    #+#             */
-/*   Updated: 2023/10/27 20:27:58 by maroth           ###   ########.fr       */
+/*   Updated: 2023/10/27 21:03:53 by maroth           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ t_list	*ft_lstlast(t_list *lst)
 	t_list	*last_node;
 
 	last_node = lst;
+	if (!lst)
+		return ((void *)0);
 	while (last_node->next)
 		last_node = last_node->next;
 	return (last_node);
